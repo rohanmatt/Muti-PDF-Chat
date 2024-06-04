c59a4645c3beac674be773d75763cc6bfc7a7a70
Rohan Roy <77683536+rohanmatt@users.noreply.github.com>
2024-06-05 01:44:51 +0530

    updates

---
+++ htmlTemplates.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://cdn.dribbble.com/users/9287887/screenshots/17042234/media/3340b90d3def591d85fb2818076ef5c5.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://cdn.dribbble.com/userupload/11712808/file/original-644e38f8d544a1b421498d4df23a3015.png">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
